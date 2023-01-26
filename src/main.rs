adab94395a14791d96734c6a7b011771280bb713
Mateusz Erwinski <101421783+AirVine64@users.noreply.github.com>
2023-01-26 17:19:12 +0100

    Change output system

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,12 @@ use std::time::Instant;
 
 fn main() {
     //Variables
+    let mut primes: Vec<i32> = Vec::new();
     let now = Instant::now();
     let mut z: i32;
 
     //Main part
-    for i in (1..100).step_by(2) {
+    for i in (1..100000).step_by(2) {
         z = 1;
         for j in 2..i+1 {
             if i%j == 0 {
@@ -18,9 +19,10 @@ fn main() {
             }
         }
         if z == 2 {
-            println!("{}", i)
+            primes.push(i);
         }
     }
+    println!("{:?}", primes);
     let elapsed = now.elapsed();
     println!("Time of execution: {:.2?}", elapsed);
 }
