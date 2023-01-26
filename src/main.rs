e6e87ab42876a0ab5a5cd40f2024f05343b9649b
Mateusz Erwinski <101421783+AirVine64@users.noreply.github.com>
2023-01-26 16:53:33 +0100

    Fix bug

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,21 @@ use std::time::Instant;
 fn main() {
     //Variables
     let now = Instant::now();
-    let mut z: i128;
+    let mut z: i32;
 
     //Main part
-    for i in (1..100000).step_by(2) {
-        z = 0;
+    for i in (1..100).step_by(2) {
+        z = 1;
         for j in 2..i+1 {
             if i%j == 0 {
                 z = z + 1;
-                if z != 1 {
-                    println!("{}", i);
+                if z == 3 {
                     break;
                 }
             }
+        }
+        if z == 2 {
+            println!("{}", i)
         }
     }
     let elapsed = now.elapsed();
