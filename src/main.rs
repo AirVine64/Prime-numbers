5f9cade420bcf48211bd535788d3ce877cc3eba2
Mateusz Erwinski <101421783+AirVine64@users.noreply.github.com>
2023-09-14 21:04:07 +0200

    Formatting changes

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use std::time::Instant;
 fn main() {
     //Variables
     let mut primes: Vec<i32> = Vec::new();
-    let now = Instant::now();
     let mut z: i32;
+    let now = Instant::now();
 
     //Main part
     for i in (1..100000).step_by(2) {
         z = 1;
         for j in 2..i+1 {
-            if i%j == 0 {
+            if i % j == 0 {
                 z = z + 1;
                 if z == 3 {
                     break;
